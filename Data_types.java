45ae927966f757beee57a9ab8cc50bb20b56ce4e
CodeByMukul-Tech <mukulvats200@gmail.com>
2025-03-24 19:21:19 +0530

    complete it

---
+++ Data_types.java
@@ -0,0 +1,45 @@
+
+public class Data_types {
+
+    public static void main(String[] args) {
+        //whole number 
+        // byte
+        // short
+        // int
+        // long
+        // decimal numbers
+        //float
+        //double
+        // character 
+        // char
+        // boolean
+        //bool 
+
+        // whole number 
+        // int age = 19;   // -2147483648 to 2147483647
+        // byte age1 = 120;  // -128 to 127 range
+        // short age2 = 34; // -32768 to 32767
+        // long age3 = 340000000l;  // 
+
+        // System.out.println(Long.MIN_VALUE);
+        // System.out.println(Long.MAX_VALUE);
+        // Decimal number
+        //    float 
+        float score = 34.11111112222f;
+        double salary = 1000.19992929292;
+        System.out.println(score);
+        System.out.println(salary);
+
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+
+
+
+        // character
+        char inital = '.';
+        // System.out.println(Char.MAX_VALUE);
+        System.out.println((int)inital);
+        String name = "Mukul";
+        System.out.println(name);
+    }
+}
