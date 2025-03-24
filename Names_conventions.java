b8be2ed26b3f1dc830932bc79d9ab2f07a2b2c78
CodeByMukul-Tech <mukulvats200@gmail.com>
2025-03-24 22:37:38 +0530

    hello

---
+++ Names_conventions.java
@@ -0,0 +1,6 @@
+public class Names_conventions {
+    public static void main(String[] args) {
+        int amount =  1100;
+        System.out.println(amount); 
+    }
+}
---
+++ maths.java
@@ -0,0 +1,22 @@
+
+public class maths {
+
+    public static void main(String[] args) {
+        // arthematic 
+        // int yoursalary = 1000;
+        // int deduction = 500;
+        // int monthly_total = yoursalary - deduction;
+        // int yearly = monthly_total * 12;
+        // int perchild = yearly / 3;
+        // System.out.println(perchild);
+
+        //    int a = 5 ;
+        //    int b = 2 ; 
+        //    float remainder = a % b;
+        //    System.out.println(remainder);
+
+        
+    
+
+    }
+}
