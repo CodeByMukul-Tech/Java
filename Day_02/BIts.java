984d3288eef3464c14974c3177c4e2c3451f86d3
CodeByMukul-Tech <mukulvats200@gmail.com>
2025-03-25 17:26:31 +0530

    Maintained the folders

--- Day_02/BIts.java
+++ Day_02/BIts.java
@@ -1,4 +1,25 @@
 package Day_02;
 
 public class BIts {
+    public static void main(String[] args) {
+//          int a = 5 ;
+//        System.out.println(Integer.toBinaryString(a));
+////        int c = ~a;
+//        System.out.println(Integer.toBinaryString(a));
+//        System.out.println(c);
+
+
+//  Println or print or printf
+        System.out.println("hello Dell");// system is package to increact it provided by jvm to increact with println.
+        System.out.println(1);
+        System.out.println("hello");
+        System.out.println('t');
+
+        System.out.println("Hello "+"Vision");
+        System.out.println("We are ready to deploy to on development and the DSA");
+        int a = 19;
+        int b = 39 ;
+        String c = "Sum";
+
+    }
 }
---
+++ Day_02/Playing_with_Strings.java
@@ -0,0 +1,9 @@
+package Day_02;
+
+public class Playing_with_Strings {
+    public static void main(String[] args) {
+        String messaage = "Hello I am using flowplus "; // sequesce of character
+        System.out.println(messaage);
+
+    }
+}
