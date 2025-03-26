e5facd54af1a8c4f7350b94fea5af29bcccb8a2e
CodeByMukul-Tech <mukulvats200@gmail.com>
2025-03-26 18:03:58 +0530

    Day 03 6:00 PM

---
+++ Day_03/strings.java
@@ -0,0 +1,13 @@
+package Day_03;
+
+public class strings {
+    public static void main(String[] args) {
+        String a = new String("Ram");
+        String b = new String("Ram");
+        System.out.println(a==b);
+        String c = "Ram";
+        String d = "Ram";
+        System.out.println(c==d);
+
+    }
+}
